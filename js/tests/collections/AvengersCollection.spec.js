21431be1817ff5a64968180db4b846a55194d264
Anthony Hastings <anthony@rehabstudio.com>
2015-02-23 16:48:30 +0000

    - Adding updated tests for the AvengersCollection.

--- js/tests/collections/AvengersCollection.spec.js
+++ js/tests/collections/AvengersCollection.spec.js
@@ -24,7 +24,47 @@ describe('The avengers collection', function() {
         expect(this.collection.url.length).to.be.above(0);
     });
 
-    it('should have parse response called', function() {
+    it('should override sync and set a specific dataType', function() {
+        var syncSpy = new sinon.spy(this.collection, 'sync');
+
+        this.collection.fetch();
+
+        // The arguments from the first call to the sync method.
+        var firstCallsFunctionArguments = syncSpy.args[0];
+
+        // From the above arguments we select the `options` argument.
+        var syncOptions = firstCallsFunctionArguments[2];
+
+        // Expecting the function only to be called once and the key to exist.
+        expect(syncSpy.calledOnce).to.be.true;
+        expect(syncOptions.dataType).to.exist;
+    });
+
+    it('should parse API responses and return objects', function() {
+        var parsedData = this.collection.parse(')]}\',[{"id":"thor","alias":"Thor"},{"id":"hulk","alias":"Hulk"}]');
+        expect(parsedData).to.be.an('array');
+        expect(parsedData.length).to.equal(2);
+    });
+
+    // By specifying a `done` callback, Mocha realises it needs to wait
+    // for it to be called in order for the test to be successful.
+    it('should fetch data correctly', function(done) {
+        var self = this;
+
+        this.timeout(4000);
+
+        this.collection.fetch({
+            success: function() {
+                if (self.collection.length > 0) {
+                    done();
+                } else {
+                    done(new Error());
+                }
+            },
+            error: function() {
+                done(new Error());
+            }
+        });
     });
 
 });
